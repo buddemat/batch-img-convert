106821e41818aff0b99e1aaca7342149300ccf2f
Matthias Budde <buddemat@users.noreply.github.com>
2023-07-23 01:27:10 +0200

    Refactor: Moved argument parsing into function.

--- batch-img-convert.py
+++ batch-img-convert.py
@@ -8,32 +8,38 @@ maintainer: Matthias Budde
 import argparse
 from pathlib import Path
 
-parser = argparse.ArgumentParser()
-argument_group = parser.add_mutually_exclusive_group()
-parser.add_argument('inpath', help='input path for image conversion', default='.')
-parser.add_argument('-r', '--recursive', help='traverse subfolders of inpath',
-                           action='store_true')
-argument_group.add_argument('-v', '--verbose', help='verbosity level (can be specified multiple times)',
-                           action='count', default=0)
-argument_group.add_argument('-q', '--quiet', help='suppress output, mutually exclusive with --verbose',
-                           action='store_true')
-args = parser.parse_args()
 
-# process args
-inpath = Path(args.inpath)
-# TODO: check if outpath exists? Or do that on creation?
-outpath = inpath / 'converted'
+def get_args():
+    '''Parse command line arguments.'''
+    opts_dict = {}
+    parser = argparse.ArgumentParser()
+    argument_group = parser.add_mutually_exclusive_group()
+    parser.add_argument('inpath', help='input path for image conversion', default='.')
+    parser.add_argument('-r', '--recursive', help='traverse subfolders of inpath',
+                               action='store_true')
+    argument_group.add_argument('-v', '--verbose', help='verbosity level (can be specified multiple times)',
+                               action='count', default=0)
+    argument_group.add_argument('-q', '--quiet', help='suppress output, mutually exclusive with --verbose',
+                               action='store_true')
+    args = parser.parse_args()
+   
+    # process args
+    opts_dict['inpath'] = Path(args.inpath)
+    # TODO: check if outpath exists? Or do that on creation?
+    opts_dict['outpath'] = opts_dict['inpath'] / 'converted'
+    
+    if args.quiet:
+        opts_dict['verbosity'] = -1
+    else:
+        opts_dict['verbosity'] = args.verbose
+    return opts_dict
 
-if args.quiet:
-    verbosity = -1
-else:
-    verbosity = args.verbose
-
-# Temporary code 
-if verbosity >= 2:
-    print(f'Running "{Path(__file__).resolve()}"...\n')
-if verbosity >= 1:
-    print(f'Input path is "{inpath.resolve()}".')
-    print(f'Output path is "{outpath.resolve()}".')
-
-
+if __name__ == '__main__':
+    
+    opts = get_args()
+    # Temporary code 
+    if opts['verbosity'] >= 2:
+        print(f'Running "{Path(__file__).resolve()}"...\n')
+    if opts['verbosity'] >= 1:
+        print(f'Input path is "{opts["inpath"].resolve()}".')
+        print(f'Output path is "{opts["outpath"].resolve()}".')
