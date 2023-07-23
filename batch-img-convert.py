60e325a539ac1fb938c069d02defd00a7dda246e
Matthias Budde <buddemat@users.noreply.github.com>
2023-07-23 02:10:32 +0200

    Feat: Added multiprocessing and progress bar.

--- batch-img-convert.py
+++ batch-img-convert.py
@@ -7,23 +7,31 @@ maintainer: Matthias Budde
 '''
 import argparse
 from pathlib import Path
+from multiprocessing import Pool, cpu_count
+import glob
+import tqdm
 
 
 def get_args():
     '''Parse command line arguments.'''
-    opts_dict = {}
+    cpucount = cpu_count()
     parser = argparse.ArgumentParser()
     argument_group = parser.add_mutually_exclusive_group()
     parser.add_argument('inpath', help='input path for image conversion', default='.')
     parser.add_argument('-r', '--recursive', help='traverse subfolders of inpath',
                                action='store_true')
+    parser.add_argument('-p', '--pool', type=int, help='poolsize for data parallelism. if option string is present without argument, then half the number of cpus are used.', const=cpucount//2, default=1, nargs='?')
     argument_group.add_argument('-v', '--verbose', help='verbosity level (can be specified multiple times)',
                                action='count', default=0)
     argument_group.add_argument('-q', '--quiet', help='suppress output, mutually exclusive with --verbose',
                                action='store_true')
     args = parser.parse_args()
    
-    # process args
+    # process args into dict
+    opts_dict = args.__dict__
+    opts_dict['poolsize'] = opts_dict.pop('pool') # rename entry
+    opts_dict['cpucount'] = cpucount
+
     opts_dict['inpath'] = Path(args.inpath)
     # TODO: check if outpath exists? Or do that on creation?
     opts_dict['outpath'] = opts_dict['inpath'] / 'converted'
@@ -31,15 +39,48 @@ def get_args():
     if args.quiet:
         opts_dict['verbosity'] = -1
     else:
-        opts_dict['verbosity'] = args.verbose
+        opts_dict['verbosity'] = opts_dict.pop('verbose')
+    opts_dict.pop('quiet')
     return opts_dict
 
+
+def convert_img(file):
+    '''Load file convert it.'''
+    with open(file, 'r') as f:
+        # TODO
+        if opts['verbosity'] >= 2:
+            print(file)
+        pass
+        return False
+
+
 if __name__ == '__main__':
-    
     opts = get_args()
-    # Temporary code 
+
+    # temporary code 
     if opts['verbosity'] >= 2:
         print(f'Running "{Path(__file__).resolve()}"...\n')
     if opts['verbosity'] >= 1:
         print(f'Input path is "{opts["inpath"].resolve()}".')
         print(f'Output path is "{opts["outpath"].resolve()}".')
+    print('')
+    
+    
+    # parallel processing
+    if opts['recursive']:
+        p = opts['inpath'].rglob('*.tif') # TODO: expand beyonf tiffs    
+    else:
+        p = opts['inpath'].glob('*.tif') # TODO: expand beyonf tiffs
+    fileslist = [f for f in p if f.is_file()]
+    filenum = len(fileslist)
+    
+    if filenum < 1:
+        print('No files found, exiting...')
+        exit()
+
+    with Pool(opts['poolsize']) as p:
+        # from https://stackoverflow.com/a/45276885/14015737
+        # the enclosing 'list()' statement waits for the iterator to end
+        # 'total' is mandatory as tqdm does not know the length of the iteration
+        r = list(tqdm.tqdm(p.imap(convert_img, fileslist), total=filenum))
+    #print(f'The sum of all lines in {filenum} .py files is {sum(r)}.')
